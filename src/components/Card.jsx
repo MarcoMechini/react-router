ecbfd92ec1900a100482ad2cfac2ce8a794b927b
Marco Mechini <mechinimarco35@gmail.com>
2025-01-09 14:46:19 +0100

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+export default function Card({ post, apiUrl, deletePost }) {
+    return (
+        <div key={post.id} className="card mb-3">
+            <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+                {post.image && (
+                    <img src={`${apiUrl}/${post.image}`} alt={post.image} className="img-fluid mb-3" />
+                )}
+                <p className="card-text">{post.content}</p>
+                <button
+                    onClick={() => deletePost(post.id)}
+                    className="btn btn-danger"
+                >
+                    Delete
+                </button>
+            </div>
+        </div>
+    )
+}
